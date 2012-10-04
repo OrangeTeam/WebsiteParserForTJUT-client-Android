ccda1062694327c863bdcca07719173a31ad3893
Bai-Jie <BaiJie1991@gmail.com>
2012-10-04 11:37:32 +0800

    call listener.onWarn before return empty List when result is empty
    修改 在因为网页已失效等原因，返回空结果前，先调用listener.onWarn方法。
    修改 补充一些错误代码的注释。

--- src/util/webpage/SchoolWebpageParser.java
+++ src/util/webpage/SchoolWebpageParser.java
@@ -489,7 +489,7 @@ public class SchoolWebpageParser {
 			if(matcher.find())
 				page = Integer.parseInt(matcher.group(1));
 			else
-				listener.onWarn(ParserListener.WARNING_CANNOT_PARSE_PAGE, "不能从计算机学院网站解析页数，现仅解析第一页内容。");
+				listener.onWarn(ParserListener.WARNING_CANNOT_PARSE_PAGE_NUMBER, "不能从计算机学院网站解析页数，现仅解析第一页内容。");
 			result.addAll(parsePostsFromSCCE(categories, start, end ,max ,doc));
 
 			for(int i = 2;i<=page;i++){
@@ -617,7 +617,7 @@ public class SchoolWebpageParser {
 			if(matcher.find())
 				page = Integer.parseInt(matcher.group(1));
 			else
-				listener.onWarn(ParserListener.WARNING_CANNOT_PARSE_PAGE, "不能从计算机学院学生网站解析页数，现仅解析第一页内容。");
+				listener.onWarn(ParserListener.WARNING_CANNOT_PARSE_PAGE_NUMBER, "不能从计算机学院学生网站解析页数，现仅解析第一页内容。");
 			
 			for(int i = 2;i<=page;i++){
 				if(max>=0 && result.size()>=max)
@@ -685,8 +685,8 @@ public class SchoolWebpageParser {
 	 */
 	public Post parsePostMainBody(Post target) throws ParserException, IOException{
 		if(target.getSource()==Post.SOURCES.UNKNOWN_SOURCE || target.getUrl()==null){
-			this.listener.onError(ParserListener.ERROR_INSUFFICIENT_INFORMATION, "Post的来源或者URL不明，无法解析正文。");
-			throw new ParserException("Post的来源或者URL不明，无法解析正文。\tPost:"+target.toString());
+			this.listener.onError(ParserListener.ERROR_INSUFFICIENT_INFORMATION, "Post的来源或者URL为null，无法解析正文。");
+			throw new ParserException("Post的来源或者URL为null，无法解析正文。\tPost:"+target.toString());
 		}
 		ReadPageHelper helper = getCurrentHelper();
 		Document doc = null;
@@ -741,8 +741,10 @@ public class SchoolWebpageParser {
 			Student studentInfoToReturn) throws ParserException, IOException{
 		try{
 			Document doc = this.getCurrentHelperAfterLogin().getWithDocument(url);
-			if(doc == null || doc.getElementsByTag("table").isEmpty())
+			if(doc == null || doc.getElementsByTag("table").isEmpty()){
+				listener.onWarn(ParserListener.WARNING_RESULT_IS_EMPTY, "结果为空，可能已选课程等页面已失效。");
 				return new LinkedList<Course>();
+			}
 			//student
 			if(studentInfoToReturn != null){
 				Pattern pattern = Pattern.compile
@@ -786,8 +788,10 @@ public class SchoolWebpageParser {
 			Student studentInfoToReturn) throws ParserException, IOException{
 		try{
 			Document doc = getCurrentHelperAfterLogin().getWithDocument(url);
-			if(doc == null || doc.getElementsByTag("table").isEmpty())
+			if(doc == null || doc.getElementsByTag("table").isEmpty()){
+				listener.onWarn(ParserListener.WARNING_RESULT_IS_EMPTY, "结果为空，可能最新成绩等页面已失效。");
 				return new LinkedList<Course>();
+			}
 			//student
 			if(studentInfoToReturn != null){
 				if(url.equals(Constant.url.ALL_PERSONAL_GRADES)){
@@ -1119,20 +1123,28 @@ public class SchoolWebpageParser {
 		public static final int ERROR_CANNOT_LOGIN = 2;
 		public static final int ERROR_IO = 3;
 		public static final int ERROR_UNSUPPORTED_ENCODING = 4;
+		/**不能读取网页中的课程（成绩）表头，无法继续解析。*/
 		public static final int ERROR_CANNOT_PARSE_TABLE_HEADING = 5;
+		/**Post的来源或者URL不明，无法解析Post正文。*/
 		public static final int ERROR_INSUFFICIENT_INFORMATION = 6;
-		public static final int WARNING_CANNOT_PARSE_PAGE = 10;
-		public static final int WARNING_UNKNOWN_COLUMN = 11;
-		public static final int WARNING_CANNOT_PARSE_DATE = 12;
-		public static final int WARNING_CANNOT_PARSE_URL = 13;
-		public static final int WARNING_CANNOT_PARSE_STRING_DATA = 14;
-		public static final int WARNING_CANNOT_PARSE_NUMBER_DATA = 15;
-		public static final int WARNINT_CANNOT_PARSE_TIME_AND_ADDRESS = 16;
-		public static final int WARNINT_CANNOT_PARSE_SEMESTER = 17;
+		/**不能读取总页面数，这样的话可能仅读取第一页*/
+		public static final int WARNING_CANNOT_PARSE_PAGE_NUMBER = 10;
+		/**结果为空，可能最新成绩、已选课程等页面已失效*/
+		public static final int WARNING_RESULT_IS_EMPTY = 11;
+		/**课程（成绩）表中遇到未知的列（数据项）。*/
+		public static final int WARNING_UNKNOWN_COLUMN = 12;
+		/**解析时间失败。*/
+		public static final int WARNING_CANNOT_PARSE_DATE = 13;
+		public static final int WARNING_CANNOT_PARSE_URL = 14;
+		public static final int WARNING_CANNOT_PARSE_STRING_DATA = 15;
+		public static final int WARNING_CANNOT_PARSE_NUMBER_DATA = 16;
+		public static final int WARNINT_CANNOT_PARSE_TIME_AND_ADDRESS = 17;
+		public static final int WARNINT_CANNOT_PARSE_SEMESTER = 18;
 		public static final int WARNING_CANNOT_PARSE_STUDENT_INFO = 20;
 		public static final int WARNING_CANNOT_PARSE_STUDENT_BIRTHDAY = 21;
 		public static final int WARNING_CANNOT_PARSE_STUDENT_ADMISSION_TIME = 22;
 		public static final int WARNING_CANNOT_PARSE_STUDENT_ACADEMIC_PERIOD = 23;
+		/**跳过/忽略某信息。*/
 		public static final int INFO_SKIP = 30;
 		/**
 		 * 当遇到错误时，调用此方法。错误意味着解释失败，停止解析。很可能此调用后解析方法抛出异常

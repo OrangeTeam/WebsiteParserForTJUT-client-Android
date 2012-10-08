2c443bbe4b0eeb2f46d4c2ff363834686551013a
Bai-Jie <BaiJie1991@gmail.com>
2012-10-08 11:36:15 +0800

    prepare release1
    删除 理工大学图标
    新增 临时文字图标
    修改 AndroidManifest中设置的图标
    修改 新增关于中图片的版权声明

--- src/org/orange/querysystem/AboutActivity.java
+++ src/org/orange/querysystem/AboutActivity.java
@@ -13,7 +13,8 @@ public class AboutActivity extends Activity{
 		
 		content = (TextView)findViewById(R.id.about);
 		content.setText("1、本软件为orange小组开发的开源软件（免费）。" + "\n" +
-						"2、本软件的网站解析功能使用了jsoup: Java HTML Parser，其许可协议为：" + "\n" +
+						"2、本早期预览版的主菜单图标来自网络，如果侵害了您的版权，请联系我们。"+"\n"+
+						"3、本软件的网站解析功能使用了jsoup: Java HTML Parser，其许可协议为：" + "\n" +
 						"The MIT License" + "\n" +
 						"Copyright © 2009 - 2012 Jonathan Hedley (jonathan@hedley.net)" + "\n" + "\n" +
 						"Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:" + "\n" + "\n" +

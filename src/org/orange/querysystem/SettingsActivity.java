36f97ba9727cbedc3e1da795498edcd34a490a21
Bai-Jie <BaiJie1991@gmail.com>
2012-12-02 13:30:35 +0800

    add some constants and summary
    新增 设置项描述和设置项的key常量

--- src/org/orange/querysystem/SettingsActivity.java
+++ src/org/orange/querysystem/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SettingsActivity extends PreferenceActivity {
+	public static final String KEY_PREF_SCHOOL_STARTING_DATE = "pref_startingDate";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {

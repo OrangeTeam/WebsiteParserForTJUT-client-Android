OrangeTeam/WebsiteParserForTJUT-client-Android

commit ac734f61d32c5111100ad6b89dfe9f6693770d55

fixes bug that ListScoresFragment cannot show decimal of score
修复 ListScoresFragment不能显示分数的小数部分的问题
优化 纠正旧代码，减少无用循环和冗余代码，简化代码，消去一点字面常量

res/layout/fragment_tabs_pager.xml
@@ -33,6 +33,7 @@
             android:id="@+id/currentTime"
             android:layout_width="fill_parent"
             android:layout_height="20dp"
+            android:text="@string/score_query"
             style="@style/wordStyle"/>
 
         <HorizontalScrollView

src/org/orange/querysystem/ScoresActivity.java
@@ -62,7 +62,6 @@ public class ScoresActivity extends FragmentActivity implements OnPostExcuteList
 	TabHost mTabHost;
 	ViewPager  mViewPager;
 	TabsAdapter mTabsAdapter;
-	private TextView currentTime;
 	public static final int PASSWORD_PROMPT = 1;
 
 	/* (non-Javadoc)
@@ -80,11 +79,13 @@ public class ScoresActivity extends FragmentActivity implements OnPostExcuteList
 		mViewPager = (ViewPager)findViewById(R.id.pager);
 
 		mTabsAdapter = new TabsAdapter(this, mTabHost, mViewPager);
-		
+
 		//3.0以上版本，使用ActionBar
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			//去掉低版本使用的Title
+			findViewById(R.id.currentTime).setLayoutParams(new LinearLayout.LayoutParams(0, 0));
 			ActionBar mActionBar = getActionBar();
-			mActionBar.setTitle("成绩单");
+			mActionBar.setTitle(R.string.score_query);
 			//横屏时，为节省空间隐藏ActionBar
 			if(getResources().getConfiguration().orientation == 
 					android.content.res.Configuration.ORIENTATION_LANDSCAPE)
@@ -219,20 +220,19 @@ public class ScoresActivity extends FragmentActivity implements OnPostExcuteList
     
     public void showScoresInfo(ArrayList<ArrayList<Course>> courses){
     	mTabsAdapter.clear();
-    	currentTime = (TextView)findViewById(R.id.currentTime);
-    	currentTime.setText("成绩单");
-    	
+
     	ArrayList<Bundle> args = new ArrayList<Bundle>(7);
-		for(int semester = 0;semester<courses.size();semester++){
+		for(ArrayList<Course> coursesInASemester:courses){
 			ArrayList<SimpleScore> scores = new ArrayList<SimpleScore>();
-			for(int counter = 0;counter<courses.get(semester).size();counter++)
-			try {
-					scores.add(new SimpleScore(courses.get(semester).get(counter).getId(), courses.get(semester).get(counter).getName(), (short)(courses.get(semester).get(counter).getTestScore()), (short)(courses.get(semester).get(counter).getTotalScore()), (float)courses.get(semester).get(counter).getGradePoint(), (byte)courses.get(semester).get(counter).getCredit(), courses.get(semester).get(counter).getKind()));
-			} catch (CourseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
-			currentTime.setText("成绩单");
+			for(Course course:coursesInASemester)
+				try {
+					scores.add(new SimpleScore(course.getId(), course.getName(),
+							course.getTestScore(), course.getTotalScore(),
+							course.getGradePoint(), course.getCredit(), course.getKind()));
+				} catch (CourseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			Bundle arg = new Bundle();
 			arg.putParcelableArrayList(ListScoresFragment.SCORES_KEY, scores);
 			args.add(arg);
@@ -242,13 +242,10 @@ public class ScoresActivity extends FragmentActivity implements OnPostExcuteList
 			TabSpec tabSpec = mTabHost.newTabSpec(counter+"学期");
 			mTabsAdapter.addTab(tabSpec.setIndicator((counter++)+"学期"),
 					ListScoresFragment.class, arg);
-			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-				currentTime.setLayoutParams(new LinearLayout.LayoutParams(0, 0));
-			}else{
+			if (Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB) {
 				mTabHost.getTabWidget().getChildAt(counter-2).setBackgroundResource(R.drawable.tab);
 			}		
 		}
-			
     }
 
 	/* (non-Javadoc)

src/org/orange/querysystem/content/ListScoresFragment.java
@@ -128,9 +128,9 @@ public class ListScoresFragment extends ListFragment {
 		/**课程名称*/
 		private String name;
 		/**结课考核成绩*/
-		private short testScore;
+		private float testScore;
 		/**期末总评成绩*/
-		private short totalScore;
+		private float totalScore;
 		/**绩点*/
 		private float gradePoint;
 		/**学分*/
@@ -147,8 +147,8 @@ public class ListScoresFragment extends ListFragment {
 		 * @param credit 学分
 		 * @param kind 课程性质
 		 */
-		public SimpleScore(int id, String name, short testScore,
-				short totalScore, float gradePoint, byte credit, String kind) {
+		public SimpleScore(int id, String name, float testScore,
+				float totalScore, float gradePoint, byte credit, String kind) {
 			super();
 			this.id = id;
 			this.name = name;
@@ -159,7 +159,7 @@ public class ListScoresFragment extends ListFragment {
 			this.kind = kind;
 		}
 		private SimpleScore(Parcel in){
-			this(in.readInt(),in.readString(),(short)in.readInt(),(short)in.readInt(),
+			this(in.readInt(),in.readString(),in.readFloat(),in.readFloat(),
 					in.readFloat(),in.readByte(),in.readString());
 		}
 
@@ -172,15 +172,16 @@ public class ListScoresFragment extends ListFragment {
 		}
 
 		public String getTestScore() {
-			if(testScore == -1){
+			//TODO 常量
+			if(Float.isNaN(testScore)){
 				return "无";
 			}else{
-				return String.valueOf(testScore);
+				return format(testScore);
 			}
 		}
 
 		public String getTotalScore() {
-			return String.valueOf(totalScore);
+			return format(totalScore);
 		}
 
 		public String getGradePoint() {
@@ -195,6 +196,13 @@ public class ListScoresFragment extends ListFragment {
 			return kind;
 		}
 
+		private static String format(float number){
+			if((int)number == number)
+				return String.valueOf((int)number);
+			else
+				return String.valueOf(number);
+		}
+
 		@Override
 		public int describeContents() {
 			return 0;
@@ -204,8 +212,8 @@ public class ListScoresFragment extends ListFragment {
 		public void writeToParcel(Parcel dest, int flags) {
 			dest.writeInt(id);
 			dest.writeString(name);
-			dest.writeInt(totalScore);
-			dest.writeInt(totalScore);
+			dest.writeFloat(totalScore);
+			dest.writeFloat(totalScore);
 			dest.writeFloat(gradePoint);
 			dest.writeByte(credit);
 			dest.writeString(kind);

src/org/orange/querysystem/util/ReadDBForScores.java
@@ -6,11 +6,9 @@ import org.orange.studentinformationdatabase.StudentInfDBAdapter;
 
 import util.webpage.Course;
 import util.webpage.Course.CourseException;
-import util.webpage.SchoolWebpageParser;
 import android.content.Context;
 import android.database.SQLException;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.Toast;
 
 public class ReadDBForScores extends AsyncTask<String,Void,ArrayList<ArrayList<Course>>>{
@@ -60,35 +58,6 @@ public class ReadDBForScores extends AsyncTask<String,Void,ArrayList<ArrayList<C
 		else{
 			Toast.makeText(context, "数据库无数据，请刷新！", Toast.LENGTH_LONG).show();			
 		}
-	}
-	
-	class MyParserListener extends SchoolWebpageParser.ParserListenerAdapter{
-
-		/* (non-Javadoc)
-		 * @see util.webpage.SchoolWebpageParser.ParserListenerAdapter#onError(int, java.lang.String)
-		 */
-		@Override
-		public void onError(int code, String message) {
-			Log.e(TAG, message);
-		}
-
-		/* (non-Javadoc)
-		 * @see util.webpage.SchoolWebpageParser.ParserListenerAdapter#onWarn(int, java.lang.String)
-		 */
-		@Override
-		public void onWarn(int code, String message) {
-			Log.w(TAG, message);
-		}
-
-		/* (non-Javadoc)
-		 * @see util.webpage.SchoolWebpageParser.ParserListenerAdapter#onInformation(int, java.lang.String)
-		 */
-		@Override
-		public void onInformation(int code, String message) {
-			Log.i(TAG, message);
-		}
 	}	
 }
-	
-	
 

commit 0b4c2a212dbd9dafc5709591617b718848c694a3

fixes bug that set text in shared layout file caused by commit ac734f61d32c5111100ad6b89dfe9f6693770d55
修复 在共享layout文件中设置ScoresActivity的标题，导致此标题串入其他Activity的问题。来源：ac734f61d32c5111100ad6b89dfe9f6693770d55

res/layout/fragment_tabs_pager.xml
@@ -33,7 +33,7 @@
             android:id="@+id/currentTime"
             android:layout_width="fill_parent"
             android:layout_height="20dp"
-            android:text="@string/score_query"
+            android:textIsSelectable="false"
             style="@style/wordStyle"/>
 
         <HorizontalScrollView

src/org/orange/querysystem/ScoresActivity.java
@@ -91,6 +91,8 @@ public class ScoresActivity extends FragmentActivity implements OnPostExcuteList
 					android.content.res.Configuration.ORIENTATION_LANDSCAPE)
 				mActionBar.hide();
 		}else{
+			//低版本使用的Title
+			((TextView)findViewById(R.id.currentTime)).setText(R.string.score_query);
 			TabWidget tabWidget = mTabHost.getTabWidget();
 			for (int i = 0; i < tabWidget.getChildCount(); i++) {  
 				View child = tabWidget.getChildAt(i);  